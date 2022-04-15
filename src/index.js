503f1a842be985168cc5068ace8922d7201fbdb2
emily doswell <emilydoswell@yahoo.co.uk>
2022-04-15 15:55:44 +0200

    init

--- src/index.js
+++ src/index.js
@@ -11,6 +11,23 @@ root.render(
   </React.StrictMode>
 );
 
+const ele = (
+	<div className = 'page'
+	style = {
+		{ textAlign: 'center' }
+	}>
+	<h1 id = 'head'> Never Stop Learning!!! </h1>
+	<h2 style = {
+		{ color: 'green' }
+	}> Because life never stops teaching </h2>
+	<p> From GeeksforGeeks </p>
+
+	</div>
+);
+
+ReactDOM.render(ele, document.getElementById('root'));
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
