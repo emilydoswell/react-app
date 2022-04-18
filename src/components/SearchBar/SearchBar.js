abef1c3359db8f8feca797819de97cb5b5a9b4cb
emily doswell <emilydoswell@yahoo.co.uk>
2022-04-18 10:51:22 +0200

    search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,6 +11,27 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOption[sortByOption];
+      <li key={sortByOptionValue}>{sortByOption}</li>;
     });
+  };
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+          <div className="SearchBar-submit">
+            <a>Let's Go</a>
+          </div>
+      </div>
+    )
   }
 }
+
+export default SearchBar;
