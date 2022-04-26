f4bfe6cfdb2785e0a79f2d02e545669248e29ea2
emily doswell <emilydoswell@yahoo.co.uk>
2022-04-26 14:13:55 +0200

    setting the search bars state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,10 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
 const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Bordertown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
@@ -17,22 +17,24 @@ const business = {
 };
 
 const businesses = [
-  business, 
-  business, 
-  business, 
-  business, 
-  business, 
+  business,
+  business,
+  business,
   business,
   business,
   business
 ];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hungry</h1>
-        <SearchBar />
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
